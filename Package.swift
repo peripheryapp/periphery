f008ce179edb1403f70ebd179da69a6a69b756b2
Yuta Saito <kateinoigakukun@gmail.com>
2020-03-29 12:56:53 +0900

    Migrate to use ArgumentParser instead of Commandant (#71)
    * Migrate check-update and help command to ArgumentParser
    
    * Migrate scan and version command
    
    * Migrate all commands
    
    * Remove Commandant dependecy
    
    * Use latest minor version
    
    * Set default value of array options
    
    * Fix test cases depending on Commandant

--- Package.swift
+++ Package.swift
@@ -11,12 +11,12 @@ let package = Package(
         .library(name: "PeripheryKit", targets: ["PeripheryKit"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ileitch/Commandant", .branch("boolean-option")),
         .package(url: "https://github.com/jpsim/SourceKitten", from: "0.29.0"),
         .package(url: "https://github.com/tuist/xcodeproj", from: "7.9.0"),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift", from: "1.3.0"),
         .package(url: "https://github.com/kylef/PathKit", from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-syntax", from: "0.50100.0")
+        .package(url: "https://github.com/apple/swift-syntax", .exact("0.50100.0")),
+        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.0.4")),
     ],
     targets: [
         .target(
@@ -26,12 +26,12 @@ let package = Package(
         .target(
             name: "PeripheryKit",
             dependencies: [
-                .product(name: "Commandant", package: "Commandant"),
                 .product(name: "SourceKittenFramework", package: "SourceKitten"),
                 .product(name: "XcodeProj", package: "xcodeproj"),
                 .product(name: "CryptoSwift", package: "CryptoSwift"),
                 .product(name: "PathKit", package: "PathKit"),
-                .product(name: "SwiftSyntax", package: "swift-syntax")
+                .product(name: "SwiftSyntax", package: "swift-syntax"),
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
             ]
         ),
         .testTarget(
--- Sources/Periphery/main.swift
+++ Sources/Periphery/main.swift
@@ -1,27 +1,28 @@
 import Foundation
-import Commandant
 import PeripheryKit
+import ArgumentParser
 
 private let logger = inject(Logger.self)
-private let registry = CommandRegistry<PeripheryKitError>()
 
-registry.register(ScanCommand())
-registry.register(ScanSyntaxCommand())
-registry.register(CheckUpdateCommand())
-registry.register(VersionCommand())
-
-let helpCommand = PeripheryKit.HelpCommand(registry: registry)
-registry.register(helpCommand)
+struct PeripheryCommand: ParsableCommand {
+    static let configuration = CommandConfiguration(
+        commandName: "periphery",
+        subcommands: [ScanCommand.self, ScanSyntaxCommand.self, CheckUpdateCommand.self, VersionCommand.self]
+    )
+}
 
 signal(SIGINT) { _ in
     Shell.terminateAll()
     exit(0)
 }
 
-registry.main(defaultVerb: helpCommand.verb) { error in
-    logger.error(error)
-
-    if let hint = error.hint {
+do {
+    let command = try PeripheryCommand.parseAsRoot()
+    try command.run()
+} catch {
+    if  let error = error as? PeripheryKitError,
+        let hint = error.hint {
         logger.hint(hint)
     }
+    PeripheryCommand.exit(withError: error)
 }
