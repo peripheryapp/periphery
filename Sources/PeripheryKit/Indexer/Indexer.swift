89a27b5732ad82760e2fd3c715bff0f06645b107
Yuta Saito <kateinoigakukun@gmail.com>
2020-03-29 12:58:30 +0900

    dynamic indexer

--- Sources/PeripheryKit/Indexer/Indexer.swift
+++ Sources/PeripheryKit/Indexer/Indexer.swift
@@ -11,22 +11,23 @@ final class Indexer {
     }
 
     static func make(buildPlan: BuildPlan, indexStore: IndexStore, graph: SourceGraph) -> Self {
-        return self.init(buildPlan: buildPlan, indexStore: indexStore, graph: graph)
+        return self.init(buildPlan: buildPlan, indexStore: indexStore, graph: graph, configuration: inject())
     }
 
     private let buildPlan: BuildPlan
     private let indexStore: IndexStore
     private let graph: SourceGraph
 
-    private let indexers: [TypeIndexer.Type] = [
-        IndexStoreIndexer.self,
-        XibIndexer.self
-    ]
+    private let indexers: [TypeIndexer.Type]
 
-    required init(buildPlan: BuildPlan, indexStore: IndexStore, graph: SourceGraph) {
+    required init(buildPlan: BuildPlan, indexStore: IndexStore, graph: SourceGraph, configuration: Configuration) {
         self.buildPlan = buildPlan
         self.indexStore = indexStore
         self.graph = graph
+        self.indexers = [
+            IndexStoreIndexer.self,
+            XibIndexer.self
+        ]
     }
 
     func perform() throws {
