5f9dc6979c801ad5fb57056c8b32a49dde2f8338
Yuta Saito <kateinoigakukun@gmail.com>
2020-10-03 17:21:42 +0900

    Update SwiftSyntax for 5.3

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .package(name: "XcodeProj", url: "https://github.com/tuist/xcodeproj", from: "7.9.0"),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift", from: "1.3.0"),
         .package(url: "https://github.com/kylef/PathKit", from: "1.0.0"),
-        .package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax", .exact("0.50200.0")),
+        .package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax", .exact("0.50300.0")),
         .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.0.4")),
         .package(url: "https://github.com/apple/swift-tools-support-core.git", .branch("master")),
     ],
