7aaef619c5331040fa3b533f6889825442b79095
Yuta Saito <kateinoigakukun@gmail.com>
2020-10-03 17:46:00 +0900

    Support Xcode12 (#109)
    * Update SourceKitten
    
    * Fix Xcode12 compiler arguments issue
    
    Reference: https://github.com/realm/SwiftLint/pull/3366
    
    * Update SwiftSyntax for 5.3
    
    * Fix compile target test case to match latest SwiftPM behavior
    
    The latest SwiftPM generate a xcodeproj target for test target and it
    doesn't inherit deployment target from main product deployment target
    version.

--- Package.swift
+++ Package.swift
@@ -11,11 +11,11 @@ let package = Package(
         .library(name: "PeripheryKit", targets: ["PeripheryKit"])
     ],
     dependencies: [
-        .package(url: "https://github.com/jpsim/SourceKitten", from: "0.29.0"),
+        .package(url: "https://github.com/jpsim/SourceKitten", from: "0.30.1"),
         .package(name: "XcodeProj", url: "https://github.com/tuist/xcodeproj", from: "7.9.0"),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift", from: "1.3.0"),
         .package(url: "https://github.com/kylef/PathKit", from: "1.0.0"),
-        .package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax", .exact("0.50200.0")),
+        .package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax", .exact("0.50300.0")),
         .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.0.4")),
         .package(url: "https://github.com/apple/swift-tools-support-core.git", .branch("master")),
     ],
