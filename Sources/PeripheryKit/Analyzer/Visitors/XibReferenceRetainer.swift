e23e745541852241a5c2eb322c063dd93c740c04
Ian Leitch <port001@gmail.com>
2020-12-01 11:58:46 +0100

    Retain @IBInspectable properties defined in a UIView extension. Closes #208

--- Sources/PeripheryKit/Analyzer/Visitors/XibReferenceRetainer.swift
+++ Sources/PeripheryKit/Analyzer/Visitors/XibReferenceRetainer.swift
@@ -6,12 +6,20 @@ final class XibReferenceRetainer: SourceGraphVisitor {
     }
 
     private let graph: SourceGraph
+    private let ibAttributes = ["IBOutlet", "IBAction", "IBInspectable"]
 
     required init(graph: SourceGraph) {
         self.graph = graph
     }
 
     func visit() {
+        retainReferencedClasses()
+        retainPropertiesDefinedInExtension()
+    }
+
+    // MARK: - Private
+
+    private func retainReferencedClasses() {
         let classes = graph.declarations(ofKind: .class)
         let referencedClasses = classes.filter { cls in
             graph.xibReferences.contains { $0.className == cls.name }
@@ -25,10 +33,20 @@ final class XibReferenceRetainer: SourceGraphVisitor {
             let allDecls = xibClass.declarations.union(superclassesDecls)
 
             for declaration in allDecls {
-                let attributes = declaration.attributes
-
-                if attributes.contains("IBOutlet") || attributes.contains("IBAction") || attributes.contains("IBInspectable") {
+                if declaration.attributes.contains(where: { ibAttributes.contains($0) }) {
                     graph.markRetained(declaration)
+                }
+            }
+        }
+    }
+
+    private func retainPropertiesDefinedInExtension() {
+        let extensions = graph.declarations(ofKind: .extensionClass)
+
+        for extDecl in extensions {
+            for decl in extDecl.declarations {
+                if decl.attributes.contains(where: { ibAttributes.contains($0) }) {
+                    graph.markRetained(decl)
                 }
             }
         }
