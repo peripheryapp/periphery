967a7e323eed48186f4955d291fc519330835e07
Ian Leitch <port001@gmail.com>
2020-12-04 13:42:39 +0100

    Allow protocols that inherit AnyObject to be marked as redundant.

---
+++ Tests/RetentionFixtures/testRedundantProtocolThatInheritsAnyObject.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+protocol FixtureProtocol120: class {}
+protocol FixtureProtocol121: AnyObject {}
+protocol FixtureProtocol122: Any {}
+
+public class FixtureClass120: FixtureProtocol120 {}
+public class FixtureClass121: FixtureProtocol121 {}
+public class FixtureClass122: FixtureProtocol122 {}
