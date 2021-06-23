d734d8be264349e89b0e0e8db3704936ef445ea4
Ian Leitch <ian@leitch.io>
2021-06-23 10:32:25 +0200

    Remove fixture causing warning about deprecated 'class' inheritance.

--- Tests/Fixtures/RetentionFixtures/testRedundantProtocolThatInheritsAnyObject.swift
+++ Tests/Fixtures/RetentionFixtures/testRedundantProtocolThatInheritsAnyObject.swift
@@ -1,9 +1,7 @@
 import Foundation
 
-protocol FixtureProtocol120: class {}
 protocol FixtureProtocol121: AnyObject {}
 protocol FixtureProtocol122: Any {}
 
-public class FixtureClass120: FixtureProtocol120 {}
 public class FixtureClass121: FixtureProtocol121 {}
 public class FixtureClass122: FixtureProtocol122 {}
