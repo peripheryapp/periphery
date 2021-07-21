72434a47acd3fe3c1c3cd2f9d6f2f55a562f70ba
Ian Leitch <ian@leitch.io>
2021-07-21 21:18:38 +0200

    Retain SwiftUI (UI/NS)ApplicationDelegateAdaptor wrapped properties. Closes #376

--- Sources/PeripheryKit/Analyzer/Visitors/EntryPointAttributeRetainer.swift
+++ Sources/PeripheryKit/Analyzer/Visitors/EntryPointAttributeRetainer.swift
@@ -12,26 +12,28 @@ final class EntryPointAttributeRetainer: SourceGraphVisitor {
     }
 
     func visit() {
-        let classes = graph.declarations(ofKinds: [.class, .struct])
-        let mainClasses = classes.filter {
-            $0.attributes.contains("NSApplicationMain") ||
-            $0.attributes.contains("UIApplicationMain") ||
-            $0.attributes.contains("main")
-        }
-
-        mainClasses.forEach {
-            graph.markRetained($0)
-
-            $0.ancestralDeclarations.forEach {
+        graph
+            .declarations(ofKinds: [.class, .struct])
+            .lazy
+            .filter {
+                $0.attributes.contains("NSApplicationMain") ||
+                $0.attributes.contains("UIApplicationMain") ||
+                $0.attributes.contains("main")
+            }
+            .forEach {
                 graph.markRetained($0)
-            }
+                graph.markMainAttributed($0)
 
-            if $0.attributes.contains("main") {
-                // @main requires a static main() function.
-                $0.declarations
-                    .filter { $0.kind == .functionMethodStatic && $0.name == "main()"}
-                    .forEach { graph.markRetained($0) }
+                $0.ancestralDeclarations.forEach {
+                    graph.markRetained($0)
+                }
+
+                if $0.attributes.contains("main") {
+                    // @main requires a static main() function.
+                    $0.declarations
+                        .filter { $0.kind == .functionMethodStatic && $0.name == "main()"}
+                        .forEach { graph.markRetained($0) }
+                }
             }
-        }
     }
 }
--- Tests/XcodeTests/SwiftUIProject/SwiftUIProject/App.swift
+++ Tests/XcodeTests/SwiftUIProject/SwiftUIProject/App.swift
@@ -2,9 +2,13 @@ import SwiftUI
 
 @main
 struct SwiftUIProjectApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
+
+class AppDelegate: NSObject, UIApplicationDelegate {}
