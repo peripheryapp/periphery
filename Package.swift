0086f4bfdead39f80118bf0316b4f64e044f91f6
Ian Leitch <ian@leitch.io>
2023-10-29 16:17:35 +0100

    Update SwiftSyntax

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ var dependencies: [Package.Dependency] = [
     .package(url: "https://github.com/tadija/AEXML", from: "4.0.0"),
     .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
     .package(url: "https://github.com/ileitch/swift-indexstore", from: "9.0.3"),
-    .package(url: "https://github.com/apple/swift-syntax", from: "509.0.0"),
+    .package(url: "https://github.com/apple/swift-syntax", from: "509.0.1"),
     .package(url: "https://github.com/ileitch/swift-filename-matcher", from: "0.0.0"),
 ]
 
