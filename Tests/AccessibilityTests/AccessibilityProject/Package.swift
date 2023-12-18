d9fec3a696493bfa4d756ea5b292d91e856d9913
Ian Leitch <ian@leitch.io>
2023-12-18 22:05:27 +0000

    Fix accessibility tests

--- Tests/AccessibilityTests/AccessibilityProject/Package.swift
+++ Tests/AccessibilityTests/AccessibilityProject/Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.9
+// swift-tools-version:5.8
 
 import PackageDescription
 
 let package = Package(
     name: "AccessibilityProject",
-    platforms: [.macOS(.v14)],
+    platforms: [.macOS(.v13)],
     products: [
         .executable(
             name: "app",
