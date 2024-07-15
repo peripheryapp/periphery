02824a7c2fff09aa1ed33542e0c890d3fe3acaab
Gorbenko Roman <45801227+rofle100lvl@users.noreply.github.com>
2024-07-15 11:35:14 +0200

    Removed CodeRemoval

--- Sources/PeripheryKit/CodeRemoval/SyntaxRemover.swift
+++
@@ -1,8 +0,0 @@
-import SwiftSyntax
-import SourceGraph
-import SyntaxAnalyse
-
-protocol SyntaxRemover {
-    init(resultLocation: Location, replacements: [String], locationBuilder: SourceLocationBuilder)
-    func perform(syntax: SourceFileSyntax) -> SourceFileSyntax
-}
