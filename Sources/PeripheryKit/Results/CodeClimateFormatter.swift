56a4d1b41bac5da9215a7768e011a9dfa2150a0f
Martin Redington <mildm8nnered@gmail.com>
2024-08-08 00:18:36 +0100

    redid updates

--- Sources/PeripheryKit/Results/CodeClimateFormatter.swift
+++ Sources/PeripheryKit/Results/CodeClimateFormatter.swift
@@ -49,7 +49,6 @@ final class CodeClimateFormatter: OutputFormatter {
         }
 
         let data = try JSONSerialization.data(withJSONObject: jsonObject, options: [.prettyPrinted, .withoutEscapingSlashes])
-        let json = String(decoding: data, as: UTF8.self)
-        return json ?? ""
+        return String(decoding: data, as: UTF8.self)
     }
 }
