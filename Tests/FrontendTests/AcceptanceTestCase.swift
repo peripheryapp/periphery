5bef2b0d2de0f55376c4eeea5658b2c9cb8d91bc
Gorbenko Roman <45801227+rofle100lvl@users.noreply.github.com>
2024-08-18 14:55:42 +0200

    Removed public

--- Tests/FrontendTests/AcceptanceTestCase.swift
+++ Tests/FrontendTests/AcceptanceTestCase.swift
@@ -1,41 +1,40 @@
 import XCTest
 import Commands
+import SystemPackage
 import ArgumentParser
 import Shared
-import System
 
-
-public enum Fixture: String {
+enum Fixture: String {
     case defaultiOSProject = "Tests/FrontendTests/DefaultiOSProject/DefaultiOSProject.xcodeproj"
 }
 
-public class AcceptanceTestCase: XCTestCase {
+class AcceptanceTestCase: XCTestCase {
     let packageRootPath = URL(fileURLWithPath: #file).pathComponents
         .prefix(while: { $0 != "Tests" }).joined(separator: "/").dropFirst()
     
-    public override class func setUp() {
+    override class func setUp() {
         Configuration.shared.reset()
         super.setUp()
     }
     
-    public override class func tearDown() {
+    override class func tearDown() {
         Configuration.shared.reset()
         super.tearDown()
     }
     
-    public func run(command: FrontendCommand.Type, arguments: String...) throws {
+    func run(command: FrontendCommand.Type, arguments: String...) throws {
         var command = try command
             .parse(arguments)
         try command.run()
     }
 
-    public func setupFixture(fixture: Fixture) -> FilePath {
+    func setupFixture(fixture: Fixture) -> FilePath {
         var file = FilePath(String(packageRootPath))
         file.append(fixture.rawValue)
         return file
     }
     
-    public func XCTOutputDefaultOutputWithoutUnusedCode(scheme: String) {
+    func XCTOutputDefaultOutputWithoutUnusedCode(scheme: String) {
         XCTAssertTrue(LoggerStorage.collectedLogs.contains(
             [
                 "* Inspecting project...",
