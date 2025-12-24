223c02c7ee996d538ac8a42f0c8694e7c7dc7745
Ian Leitch <ian@leitch.io>
2025-12-24 13:47:10 +0100

    Add test for #805

---
+++ Tests/Fixtures/Sources/UnusedImportFixtures/C/ConformanceFunction.swift
@@ -0,0 +1,9 @@
+// Module C: Provides the function that accepts ConformanceProtocol
+
+import UnusedImportFixtureA
+
+/// A function that accepts a ConformanceProtocol conforming type
+public func acceptConformingType(_ value: ConformanceProtocol) -> Int {
+    value.conformanceProperty
+}
+
