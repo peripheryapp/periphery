7dc47a7b10cc8b33212718bb789ec65966b1102a
Dan Wood <207080+danwood@users.noreply.github.com>
2025-12-30 13:23:53 -0800

    Revert "Remove files that got moved"
    This reverts commit 882e58d4d95665bd061bf516350be66cab1225b6.

---
+++ Tests/AccessibilityTests/RedundantFilePrivateAccessibilityTest.swift
@@ -0,0 +1,21 @@
+import Configuration
+@testable import TestShared
+import XCTest
+
+final class RedundantFilePrivateAccessibilityTest: SPMSourceGraphTestCase {
+    override static func setUp() {
+        super.setUp()
+        build(projectPath: AccessibilityProjectPath)
+    }
+
+    func testRedundantFilePrivateClass() {
+        index()
+        assertRedundantFilePrivateAccessibility(.class("RedundantFilePrivateClass"))
+    }
+
+    func testNotRedundantFilePrivateClass() {
+        index()
+        assertNotRedundantFilePrivateAccessibility(.class("NotRedundantFilePrivateClass"))
+    }
+}
+ 
